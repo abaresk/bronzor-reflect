94301d314c54f903163b7dc089956cabea724fc7
Abaresk <abaresk01@gmail.com>
2021-12-20 15:30:23 +0000

    Add Wallet component

---
+++ src/app/wallet/wallet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { WalletService } from '../wallet.service';
+
+@Component({
+  selector: 'game-wallet',
+  templateUrl: './wallet.component.html',
+  styleUrls: ['./wallet.component.scss']
+})
+export class WalletComponent implements OnInit {
+
+  constructor(public walletService: WalletService) { }
+
+  ngOnInit(): void {
+  }
+}

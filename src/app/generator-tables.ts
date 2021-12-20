9808eab7acad218c386897b2a0a9d10a12b27fb8
Abaresk <abaresk01@gmail.com>
2021-12-20 15:26:56 +0000

    Add GeneratorService

---
+++ src/app/generator-tables.ts
@@ -0,0 +1,297 @@
+import { Beam, BeamPrize, beamPrizePayouts, BombPrize, BombType, InventoryPrize, InventoryPrizeType, jackpotPrize, largeSumPrize, mediumSumPrize, MoneyPrize, MoneyPrizeType, normalBombPrize, plus1BeamPrize, Prize, prizes, smallSumPrize } from "./items";
+
+interface Range {
+    min: number; // inclusive
+    max: number; // inclusive
+};
+
+type LevelYield = ReadonlyMap<number, Range>;
+
+// Always 1 Jackpot per round
+const jackpotsByLevel: LevelYield = new Map([
+    [1, { min: 1, max: 1 }],
+    [2, { min: 1, max: 1 }],
+    [3, { min: 1, max: 1 }],
+    [4, { min: 1, max: 1 }],
+    [5, { min: 1, max: 1 }],
+    [6, { min: 1, max: 1 }],
+    [7, { min: 1, max: 1 }],
+    [8, { min: 1, max: 1 }],
+]);
+
+const largeSumsByLevel: LevelYield = new Map([
+    [1, { min: 1, max: 1 }],
+    [2, { min: 1, max: 1 }],
+    [3, { min: 1, max: 2 }],
+    [4, { min: 1, max: 2 }],
+    [5, { min: 1, max: 2 }],
+    [6, { min: 2, max: 3 }],
+    [7, { min: 2, max: 3 }],
+    [8, { min: 2, max: 4 }],
+]);
+
+const mediumSumsByLevel: LevelYield = new Map([
+    [1, { min: 1, max: 3 }],
+    [2, { min: 1, max: 3 }],
+    [3, { min: 2, max: 4 }],
+    [4, { min: 2, max: 4 }],
+    [5, { min: 3, max: 5 }],
+    [6, { min: 3, max: 5 }],
+    [7, { min: 4, max: 6 }],
+    [8, { min: 4, max: 6 }],
+]);
+
+const smallSumsByLevel: LevelYield = new Map([
+    [1, { min: 3, max: 5 }],
+    [2, { min: 3, max: 5 }],
+    [3, { min: 3, max: 5 }],
+    [4, { min: 4, max: 6 }],
+    [5, { min: 4, max: 6 }],
+    [6, { min: 4, max: 6 }],
+    [7, { min: 4, max: 7 }],
+    [8, { min: 4, max: 7 }],
+]);
+
+const plus1BeamsByLevel: LevelYield = new Map([
+    [1, { min: 0, max: 1 }],
+    [2, { min: 0, max: 1 }],
+    [3, { min: 0, max: 1 }],
+    [4, { min: 0, max: 2 }],
+    [5, { min: 0, max: 2 }],
+    [6, { min: 0, max: 3 }],
+    [7, { min: 1, max: 4 }],
+    [8, { min: 1, max: 4 }],
+]);
+
+const plus3BeamsByLevel: LevelYield = new Map([
+    [1, { min: 0, max: 0 }],
+    [2, { min: 0, max: 0 }],
+    [3, { min: 0, max: 0 }],
+    [4, { min: 0, max: 1 }],
+    [5, { min: 0, max: 1 }],
+    [6, { min: 0, max: 1 }],
+    [7, { min: 0, max: 2 }],
+    [8, { min: 0, max: 2 }],
+]);
+
+const minus1BeamsByLevel: LevelYield = new Map([
+    [1, { min: 0, max: 0 }],
+    [2, { min: 0, max: 0 }],
+    [3, { min: 0, max: 1 }],
+    [4, { min: 0, max: 1 }],
+    [5, { min: 0, max: 1 }],
+    [6, { min: 0, max: 1 }],
+    [7, { min: 0, max: 2 }],
+    [8, { min: 0, max: 2 }],
+]);
+
+const cometBeamsByLevel: LevelYield = new Map([
+    [1, { min: 0, max: 0 }],
+    [2, { min: 0, max: 0 }],
+    [3, { min: 0, max: 0 }],
+    [4, { min: 0, max: 1 }],
+    [5, { min: 0, max: 1 }],
+    [6, { min: 0, max: 1 }],
+    [7, { min: 0, max: 2 }],
+    [8, { min: 0, max: 2 }],
+]);
+
+const flameBeamsByLevel: LevelYield = new Map([
+    [1, { min: 0, max: 0 }],
+    [2, { min: 0, max: 0 }],
+    [3, { min: 0, max: 1 }],
+    [4, { min: 0, max: 1 }],
+    [5, { min: 0, max: 2 }],
+    [6, { min: 0, max: 2 }],
+    [7, { min: 0, max: 3 }],
+    [8, { min: 0, max: 3 }],
+]);
+
+const phaseBeamsByLevel: LevelYield = new Map([
+    [1, { min: 0, max: 0 }],
+    [2, { min: 0, max: 0 }],
+    [3, { min: 0, max: 0 }],
+    [4, { min: 0, max: 0 }],
+    [5, { min: 0, max: 1 }],
+    [6, { min: 0, max: 1 }],
+    [7, { min: 0, max: 1 }],
+    [8, { min: 0, max: 1 }],
+]);
+
+const doublePrizeBeamsByLevel: LevelYield = new Map([
+    [1, { min: 0, max: 0 }],
+    [2, { min: 0, max: 0 }],
+    [3, { min: 0, max: 1 }],
+    [4, { min: 0, max: 1 }],
+    [5, { min: 0, max: 2 }],
+    [6, { min: 0, max: 2 }],
+    [7, { min: 0, max: 2 }],
+    [8, { min: 0, max: 3 }],
+]);
+
+const waterBeamsByLevel: LevelYield = new Map([
+    [1, { min: 0, max: 0 }],
+    [2, { min: 0, max: 1 }],
+    [3, { min: 0, max: 1 }],
+    [4, { min: 0, max: 1 }],
+    [5, { min: 0, max: 1 }],
+    [6, { min: 0, max: 2 }],
+    [7, { min: 0, max: 2 }],
+    [8, { min: 0, max: 2 }],
+]);
+
+const normalBombsByLevel: LevelYield = new Map([
+    [1, { min: 2, max: 2 }],
+    [2, { min: 2, max: 3 }],
+    [3, { min: 2, max: 3 }],
+    [4, { min: 2, max: 4 }],
+    [5, { min: 2, max: 4 }],
+    [6, { min: 2, max: 5 }],
+    [7, { min: 3, max: 5 }],
+    [8, { min: 4, max: 6 }],
+]);
+
+export function getYieldRange(prize: Prize, level: number): Range {
+    const defaultRange: Range = { min: 0, max: 0 };
+
+    let levelYield: LevelYield = new Map();
+    if (prize as MoneyPrize) {
+        switch (prize.type) {
+            case MoneyPrizeType.Jackpot:
+                levelYield = jackpotsByLevel;
+                break;
+            case MoneyPrizeType.LargeSum:
+                levelYield = largeSumsByLevel;
+                break;
+            case MoneyPrizeType.MediumSum:
+                levelYield = mediumSumsByLevel;
+                break;
+            case MoneyPrizeType.SmallSum:
+                levelYield = smallSumsByLevel;
+                break;
+        }
+    } else if (prize as InventoryPrize) {
+        switch (prize.type) {
+            case InventoryPrizeType.Plus1Beam:
+                levelYield = plus1BeamsByLevel;
+                break;
+            case InventoryPrizeType.Plus3Beams:
+                levelYield = plus3BeamsByLevel;
+                break;
+            case InventoryPrizeType.Minus1Beam:
+                levelYield = minus1BeamsByLevel;
+                break;
+        }
+    } else if (prize as BeamPrize) {
+        switch (prize.type) {
+            case Beam.Comet:
+                levelYield = cometBeamsByLevel;
+                break;
+            case Beam.Flame:
+                levelYield = flameBeamsByLevel;
+                break;
+            case Beam.Phase:
+                levelYield = phaseBeamsByLevel;
+                break;
+            case Beam.DoublePrize:
+                levelYield = doublePrizeBeamsByLevel;
+                break;
+            case Beam.Water:
+                levelYield = waterBeamsByLevel;
+                break;
+        }
+    } else if (prize as BombPrize) {
+        switch (prize.type) {
+            case BombType.Normal:
+                levelYield = normalBombsByLevel;
+                break;
+        }
+    }
+
+    return levelYield.get(level) ?? defaultRange;
+}
+
+function getTotalRange1StdDev(level: number): Range {
+    let totalAvg = 0;
+    let totalVariance = 0;
+    for (let prize of prizes) {
+        const range = getYieldRange(prize, level);
+
+        const avg = (range.min + range.max) / 2;
+        totalAvg += avg;
+
+        // See https://en.wikipedia.org/wiki/Discrete_uniform_distribution.
+        const variance = Math.pow(range.max + 1 - range.min, 2) / 12;
+        // Total variance can be summed since each random variable is independent:
+        // https://www.probabilitycourse.com/chapter6/6_1_2_sums_random_variables.php.
+        totalVariance += variance;
+    }
+
+    const stdDev = Math.sqrt(totalVariance);
+    return { min: totalAvg - stdDev, max: totalAvg + stdDev };
+}
+
+export function getTotalRange(level: number): Range {
+    const stdDevRange = getTotalRange1StdDev(level);
+    return { min: Math.floor(stdDevRange.min), max: Math.floor(stdDevRange.max) };
+}
+
+const levelsArr = Array.from({ length: 8 }, (_, i) => i + 1);
+export const totalByLevel: LevelYield =
+    new Map(levelsArr.map((level) => [level, getTotalRange(level)]));
+
+export function getProbUnreachable(prize: Prize): number {
+    if (prize as MoneyPrize) {
+        switch (prize.type) {
+            case MoneyPrizeType.Jackpot:
+                return 0;
+            case MoneyPrizeType.LargeSum:
+                return 0.2;
+            case MoneyPrizeType.MediumSum:
+                return 0.3;
+            case MoneyPrizeType.SmallSum:
+                return 0.4;
+        }
+    } else if (prize as InventoryPrize) {
+        switch (prize.type) {
+            case InventoryPrizeType.Plus1Beam:
+                return 0.2;
+            case InventoryPrizeType.Plus3Beams:
+                return 0.2;
+            case InventoryPrizeType.Minus1Beam:
+                return 0.2;
+        }
+    } else if (prize as BeamPrize) {
+        switch (prize.type) {
+            case Beam.Comet:
+                return 0.2;
+            case Beam.Flame:
+                return 0.2;
+            case Beam.Phase:
+                return 0.2;
+            case Beam.DoublePrize:
+                return 0.2;
+            case Beam.Water:
+                return 0.2;
+        }
+    } else if (prize as BombPrize) {
+        switch (prize.type) {
+            case BombType.Normal:
+                return 0.2
+        }
+    }
+
+    return 0;
+}
+
+// Number of hidden Bronzors in each level
+export const hiddenBronzorsByLevel: ReadonlyMap<number, number> = new Map([
+    [1, 0],
+    [2, 1],
+    [3, 1],
+    [4, 2],
+    [5, 2],
+    [6, 3],
+    [7, 3],
+    [8, 4],
+]);
---
+++ src/app/generator.service.ts
@@ -0,0 +1,103 @@
+import { Injectable } from '@angular/core';
+import { BeamPointType, BoardConfig, BoardHistory, Bronzor } from './board';
+import { BoardService } from './board.service';
+import { Coord, Grid } from './coord';
+import { hiddenBronzorsByLevel } from './generate-tables';
+import { Beam, PrizeState } from './items';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GeneratorService {
+  config: BoardConfig = {} as BoardConfig;
+  grid: Grid = {} as Grid;
+
+  constructor(private boardService: BoardService) {
+  }
+
+  generateBoard(config: BoardConfig, level: number): void {
+    this.config = config;
+    this.grid = new Grid(config.length, config.length)
+
+    const history: BoardHistory = { beamPaths: [] };
+    const board = {
+      config: this.config,
+      bronzors: [],
+      prizes: [],
+      history: history
+    };
+    this.boardService.new(board);
+
+    const bronzors = this.placeBronzors(level);
+    this.boardService.board.bronzors = bronzors;
+
+    const prizes = this.placePrizes(bronzors, level);
+    this.boardService.board.prizes = prizes;
+  }
+
+  // TODO: Factor in level when placing Bronzors.
+  private placeBronzors(level: number): Bronzor[] {
+    const bronzors = [];
+    const hiddenBronzors = hiddenBronzorsByLevel.get(level) ?? 0;
+    const length = this.config.length;
+    const bronzorCount = this.config.bronzorCount;
+
+    // For now, placement is completely random.
+    const tiles = [...Array(length * length).keys()];
+    const shuffled = tiles.sort(() => 0.5 - Math.random());
+    const bronzorLocations = shuffled.slice(0, bronzorCount);
+
+    for (let i = 0; i < hiddenBronzors; i++) {
+      const location = bronzorLocations[i];
+      const coord = new Coord(Math.floor(location / length), location % length);
+      bronzors.push({ coord: coord, active: true, visible: false });
+    }
+
+    for (let i = hiddenBronzors; i < bronzorCount; i++) {
+      const location = bronzorLocations[i];
+      const coord = new Coord(Math.floor(location / length), location % length);
+      bronzors.push({ coord: coord, active: true, visible: true });
+    }
+    return bronzors;
+  }
+
+  private placePrizes(bronzors: Bronzor[], level: number): PrizeState[] {
+    const allCoordsSet = this.coordSet(this.ioCoords());
+    const reachableSet = this.coordSet(this.reachableCoords());
+
+    // TODO: Finish prize generation algorithm.
+
+    return [];
+  }
+
+  private reachableCoords(): Coord[] {
+    const reachable = [];
+    const ioCoords = this.ioCoords();
+    for (let coord of ioCoords) {
+      // Fire a beam into the board from this coord and record where it is
+      // emitted.
+      const beamPath = this.boardService.fireBeam(Beam.Normal, coord, true);
+      if (!beamPath) continue;
+
+      const lastPoint = beamPath.path[beamPath.path.length - 1];
+      if (lastPoint.type !== BeamPointType.Emit) continue;
+
+      reachable.push(lastPoint.coord);
+    }
+    return reachable;
+  }
+
+  // All coordinates surrounding the board where a beam can be fired or
+  // emitted.
+  private ioCoords(): Coord[] {
+    const edgeSegments = this.grid.edgeSegments(1);
+    const coords = edgeSegments.map((segment) => segment.allCoords());
+    return coords.flat(2);
+  }
+
+  private coordSet(coords: Coord[]): Set<string> {
+    const set: Set<string> = new Set();
+    coords.forEach((coord) => set.add(coord.toString()))
+    return set;
+  }
+}

26f237403986198fb799362ce7888d786fdbce0d
Abaresk <abaresk01@gmail.com>
2021-12-30 03:19:11 +0000

    Add function to sleep for N ticks

--- src/app/util/timing.ts
+++ src/app/util/timing.ts
@@ -1,3 +1,9 @@
-export async function sleep(delayMs: number) {
+import { TICK } from "../common/tick";
+
+export async function sleepMs(delayMs: number) {
     return new Promise((resolve) => { setTimeout(resolve, delayMs); });
+}
+
+export async function sleepTicks(delayTicks: number) {
+    await sleepMs(delayTicks * TICK);
 }

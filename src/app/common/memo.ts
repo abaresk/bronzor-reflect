336541e0c39ccef66589c35dfa54c4347cf78c78
Abaresk <abaresk01@gmail.com>
2022-03-16 04:02:28 +0000

    Add note to consider MAYBE_BRONZOR MemoState

--- src/app/common/memo.ts
+++ src/app/common/memo.ts
@@ -1,17 +1,19 @@
+// TODO: Consider adding another state: MARKED_MAYBE_BRONZOR. This allows the
+// player to differentiate potential Bronzor locations from certain Bronzors.
 export enum MemoState {
-    UNMARKED,
-    MARKED_NOT_BRONZOR,
-    MARKED_BRONZOR,
+  UNMARKED,
+  MARKED_NOT_BRONZOR,
+  MARKED_BRONZOR,
 };
 
 export const MEMO_STATES: ReadonlyArray<MemoState> = [
-    MemoState.UNMARKED,
-    MemoState.MARKED_NOT_BRONZOR,
-    MemoState.MARKED_BRONZOR,
+  MemoState.UNMARKED,
+  MemoState.MARKED_NOT_BRONZOR,
+  MemoState.MARKED_BRONZOR,
 ];
 
 export const MEMO_DISLAYS: ReadonlyMap<string, string> = new Map([
-    [MemoState.UNMARKED.toString(), ''],
-    [MemoState.MARKED_NOT_BRONZOR.toString(), 'x'],
-    [MemoState.MARKED_BRONZOR.toString(), 'b'],
+  [MemoState.UNMARKED.toString(), ''],
+  [MemoState.MARKED_NOT_BRONZOR.toString(), 'x'],
+  [MemoState.MARKED_BRONZOR.toString(), 'b'],
 ]);

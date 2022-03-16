361cb9d56082ad225679b42a12ad121f619e0394
Abaresk <abaresk01@gmail.com>
2022-03-16 12:32:01 +0000

    Let player skip coin merge with START button

--- src/app/services/wallet/wallet.service.ts
+++ src/app/services/wallet/wallet.service.ts
@@ -1,18 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { TICK } from '../../common/tick';
 import { Wallet } from '../../common/wallet';
+import { InputAdapterService } from '../input-adapter/input-adapter.service';
+import { GbaInput } from '../input-adapter/inputs';
 
 // Number of coins merged per tick.
 const DEFAULT_COINS_PER_TICK = 1;
+
+enum MergeSpeed {
+  Normal,
+  Fast,
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class WalletService {
   wallet = {} as Wallet;
-  coinsPerTick: number = DEFAULT_COINS_PER_TICK;
+  mergeSpeed = MergeSpeed.Normal;
 
-  constructor() { }
+  inputObservable: Subscription;
+
+  constructor(
+    private inputAdapterService: InputAdapterService
+  ) {
+    this.inputObservable = this.inputAdapterService.inputSubject
+      .subscribe((input) => { this.handleInput(input); });
+  }
 
   new(credit: number) {
     this.wallet.credit = credit;
@@ -31,12 +46,9 @@ export class WalletService {
     this.wallet.payout = payout;
   }
 
-  setFastMergeSpeed() {
-    this.coinsPerTick = DEFAULT_COINS_PER_TICK * 2;
-  }
-
-  setRegularMergeSpeed() {
-    this.coinsPerTick = DEFAULT_COINS_PER_TICK;
+  coinsPerTick() {
+    return (this.mergeSpeed === MergeSpeed.Fast) ?
+      2 * DEFAULT_COINS_PER_TICK : DEFAULT_COINS_PER_TICK;
   }
 
   // Incrementally add payout to credit. Resolves when the merge is finished.
@@ -47,11 +59,19 @@ export class WalletService {
           clearInterval(intervalId);
           resolve(undefined);
         } else {
-          const transfer = Math.min(this.wallet.payout, this.coinsPerTick);
+          const transfer = Math.min(this.wallet.payout, this.coinsPerTick());
           this.wallet.payout -= transfer;
           this.wallet.credit += transfer;
         }
       }, TICK);
     });
+  }
+
+  private handleInput(input: GbaInput) {
+    // Skip the coin merge
+    if (input === GbaInput.Start) {
+      this.wallet.credit += this.wallet.payout;
+      this.wallet.payout = 0;
+    }
   }
 }
